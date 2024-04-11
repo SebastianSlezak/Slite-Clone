98bc2f335cc0fff89066675e55db4d015bdfd233
Sebastian <szalassje@gmail.com>
2024-04-11 21:53:00 +0200

    Add dynamic header shrink effect on scroll

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
